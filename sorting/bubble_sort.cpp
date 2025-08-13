ae010835771f91754937e0609e7c7d25e5c2e96f
Jerry Chen <jerryvchen1@gmail.com>
2025-08-13 09:34:18 -0700

    sorting: bubble_sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void bubble_sort(vector<int>& a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    // printf("%.4f\n", x);
+
+    return 0;
+}
